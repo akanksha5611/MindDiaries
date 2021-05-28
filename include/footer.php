e1757cc12389a98ff582e5ded2e50c998bcb98c0
akanksha5611 <76515386+akanksha5611@users.noreply.github.com>
2021-05-28 21:02:23 +0530

    index

---
+++ include/footer.php
@@ -0,0 +1,34 @@
+<footer class="complete-footer mainblock">
+      <div class="container">
+        <div class="footer-top mainblock">
+          <div class="footer-middle">
+              <h2>Little Angel Foundation</h2>
+              <p>Lake View CHS, H2, 3rd floor, room no 2, Sector 14 Airol, Navi Mumbai, 400708.</p>
+<p><strong>:Study Center:</strong><br>Shop No 16, Shraddha Suman CHS, Sector 20 Airol, Navi Mumbai, 400708.</p>
+            <ul>
+              <li><a href="mailto:info@littleangelfoundation.org">info@littleangelfoundation.org</a></li>
+              <li>+91 8779950245</li>
+            </ul>
+            <ul class="footer-botm-social">
+              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-bottom mainblock">
+          <div class="fb-left">
+            <p>Copyright © 2019 Little Angel Foundation. All rights reserved.</p>
+          </div>
+          <div class="fb-right">
+            <ul>
+              <li><a href="http://littleangelfoundation.org/">Home</a> </li>
+              <li><a href="http://littleangelfoundation.org/about-us.php">About Us</a> </li>
+              <li><a href="http://littleangelfoundation.org/donation.php">Donate</a> </li>
+              <li><a href="http://littleangelfoundation.org/contact-us.php">Contact Us</a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
